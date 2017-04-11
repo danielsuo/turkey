3f602358e07ee60bb8a60cd5a8d1d45caed41b4d
Daniel Suo <danielsuo@gmail.com>
2017-04-10 23:31:03 -0400

    Update dummy to use turkey client

--- apps/dummy/main.cpp
+++ apps/dummy/main.cpp
@@ -5,9 +5,9 @@
 
 int main(int argc, char *argv[]) {
   fprintf(stderr, "CLIENT LOGS\n");
-  // TURKEY *client = turkey_init();
-  //
-  // turkey_destroy(client);
+  TURKEY *client = turkey_init();
+
+  turkey_destroy(client);
 
   fprintf(stderr, "CLONE_NEWCGROUP %d\n", CLONE_NEWCGROUP);
 
