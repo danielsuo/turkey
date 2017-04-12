c7bf17feead59da75375433ea6c8b1e2cbe798ed
suo <michaelhsuo@gmail.com>
2017-04-12 02:28:23 -0400

    client library first cut

--- src/Client.h
+++ src/Client.h
@@ -9,5 +9,10 @@ public:
 
   Client(const Client&) = delete;
   Client& operator=(const Client&) = delete;
-}
+
+private:
+  std::unique_ptr<boost::interprocess::managed_shared_memory> segment_;
+  std::unique_ptr<boost::interprocess::named_mutex> mutex_;
+  std::unique_ptr<RecommendationMap> recommendationMap_;
+};
 }
