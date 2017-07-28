f96dece5f49cb6fb22685e3976aa3e0b9402405c
Daniel Suo <danielsuo@gmail.com>
2017-07-28 10:07:40 -0400

    Formatting

--- benchmarks/job.py
+++ benchmarks/job.py
@@ -5,32 +5,32 @@ import subprocess
 
 # Run a single task
 class Task:
-  def __init__(self, desc, out_dir='out', in_dir='.', executable=None, output_to_stdout=False, TURKEY_HOME='.'):
-    self.start     = desc[0]
-    self.id        = desc[1]
-    self.app       = desc[2]
-    self.conf_name = desc[3]
-    self.mode      = desc[4]
-    self.threads   = desc[5]
+    def __init__(self, desc, out_dir='out', in_dir='.', executable=None, output_to_stdout=False, TURKEY_HOME='.'):
+        self.start     = desc[0]
+        self.id        = desc[1]
+        self.app       = desc[2]
+        self.conf_name = desc[3]
+        self.mode      = desc[4]
+        self.threads   = desc[5]
 
-    self.in_dir    = in_dir
-    self.out_dir   = out_dir
-    os.system('mkdir -p %s' % self.out_dir)
+        self.in_dir    = in_dir
+        self.out_dir   = out_dir
+        os.system('mkdir -p %s' % self.out_dir)
 
-    self.app_dir = os.path.join(self.in_dir, 'apps', self.app)
-    self.exec_dir = os.path.join(TURKEY_HOME, 'build/apps', self.app)
-    self.conf_file = os.path.join(self.app_dir, 'conf', '%s.json' % self.conf_name)
+        self.app_dir = os.path.join(self.in_dir, 'apps', self.app)
+        self.exec_dir = os.path.join(TURKEY_HOME, 'build/apps', self.app)
+        self.conf_file = os.path.join(self.app_dir, 'conf', '%s.json' % self.conf_name)
 
-    with open(self.conf_file, 'r') as conf_file:
-        self.conf = json.load(conf_file)
+        with open(self.conf_file, 'r') as conf_file:
+            self.conf = json.load(conf_file)
 
-    self.out_file = os.path.join(self.out_dir, 'task.out')
-    self.output_to_stdout = output_to_stdout
+        self.out_file = os.path.join(self.out_dir, 'task.out')
+        self.output_to_stdout = output_to_stdout
 
-    if 'exe' in self.conf:
-        self.executable = os.path.join(self.exec_dir, self.conf['exe'])
-    else:
-        self.executable = os.path.join(self.exec_dir, '%s_%s' % (executable or self.app, self.mode))
+        if 'exe' in self.conf:
+            self.executable = os.path.join(self.exec_dir, self.conf['exe'])
+        else:
+            self.executable = os.path.join(self.exec_dir, '%s_%s' % (executable or self.app, self.mode))
 
     def run(self, threads=None, time_run=True, wait=False, copy_data=True):
         print('Running %s, output to %s' % (self.executable, self.out_file))
@@ -81,37 +81,37 @@ class Task:
 
 # Run a timeline of tasks
 class Job:
-  def __init__(self, args):
-    self.prefix      = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
-    self.file        = args.file
-    self.working_dir = args.working_dir
+    def __init__(self, args):
+        self.prefix      = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+        self.file        = args.file
+        self.working_dir = args.working_dir
 
-    self.out_dir     = args.out_dir if args.out_dir != None else \
-    self.prefix + '_' + self.file.split('/')[-1].split('.')[0] + '.out'
-    self.out_dir     = os.path.join(self.working_dir, self.out_dir)
-    self.time        = args.time
+        self.out_dir     = args.out_dir if args.out_dir != None else \
+        self.prefix + '_' + self.file.split('/')[-1].split('.')[0] + '.out'
+        self.out_dir     = os.path.join(self.working_dir, self.out_dir)
+        self.time        = args.time
 
-    os.system('mkdir -p %s' % os.path.join(self.working_dir, self.out_dir))
+        os.system('mkdir -p %s' % os.path.join(self.working_dir, self.out_dir))
 
-    with open(self.file, 'r') as file:
-        tasks = [task.strip().split(',') for task in file.readlines()]
+        with open(self.file, 'r') as file:
+            tasks = [task.strip().split(',') for task in file.readlines()]
 
-    self.tasks       = {}
-    self.task_array  = []
-    self.ntasks      = len(tasks)
+        self.tasks       = {}
+        self.task_array  = []
+        self.ntasks      = len(tasks)
 
-    for tid in range(len(tasks)):
-        start = tasks[tid][0]
-        if start not in self.tasks:
-            self.tasks[start] = []
+        for tid in range(len(tasks)):
+            start = tasks[tid][0]
+            if start not in self.tasks:
+                self.tasks[start] = []
 
-        task = tasks[tid]
-        task.insert(1, str(tid))
-        out_dir = os.path.join(self.out_dir, '%s_%s_%s_%s_%s_%s' % tuple(task))
+            task = tasks[tid]
+            task.insert(1, str(tid))
+            out_dir = os.path.join(self.out_dir, '%s_%s_%s_%s_%s_%s' % tuple(task))
 
-        task = Task(tasks[tid], out_dir, args.in_dir, TURKEY_HOME=args.turkey_home)
-        self.tasks[start].append(task)
-        self.task_array.append(task)
+            task = Task(tasks[tid], out_dir, args.in_dir, TURKEY_HOME=args.turkey_home)
+            self.tasks[start].append(task)
+            self.task_array.append(task)
 
     def run(self):
         tasks_run = 0
