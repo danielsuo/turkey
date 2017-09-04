fbda9f70ef486e0cf1e8edc31d2dbce67fbaed39
Daniel Suo <danielsuo@gmail.com>
2017-09-04 02:19:56 +0000

    Move stuff around

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ from pip.req import parse_requirements
 install_reqs = parse_requirements('requirements.txt', session='hack')
 
 # reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='turkey-benchmarks',
+    name='turkey',
     version='0.0.1',
-    description='Turkey Scheduler',
+    description='Turkey',
     author='Daniel Suo',
     author_email='dsuo@cs.princeton.edu',
     url='https://github.com/danielsuo/turkey',
-    packages=['benchmarks'],
+    packages=['turkey'],
     install_requires=reqs)
