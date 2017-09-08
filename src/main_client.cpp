8d6c62d4e86d3328e2810f1ebf6e9fc781a27204
Daniel Suo <danielsuo@gmail.com>
2017-09-08 03:50:22 +0000

    Add yasm, zmq, czmq, and cppzmq

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -8,10 +8,10 @@ using namespace Turkey;
 int main(int argc, char* argv[]) {
   using namespace std::chrono_literals;
   std::cout << "Starting client" << std::endl;
-  Client client(1);
+  // Client client(1);
   while (true) {
     std::this_thread::sleep_for(10s);
-    LOG(INFO) << client.pollServer();
+    // LOG(INFO) << client.pollServer();
   }
   return 0;
 }
