c754eda618448f1004295ac3e11f59236b060846
Daniel Suo <danielsuo@gmail.com>
2017-09-23 14:17:19 +0000

    Remove old queues

--- apps/dedup/src/turkey/encoder.cpp
+++ apps/dedup/src/turkey/encoder.cpp
@@ -42,9 +42,9 @@ extern "C" {
 #include "tree.h"
 }
 
-#include <folly/MPMCQueue.h>
 #include <glog/logging.h>
 #include "Client.h"
+#include "mpmc.h"
 
 #ifdef ENABLE_GZIP_COMPRESSION
 #include <zlib.h>
@@ -63,64 +63,6 @@ folly::MPMCQueue<void*> queue_refine(QUEUE_SIZE);
 folly::MPMCQueue<void*> queue_deduplicate(QUEUE_SIZE);
 folly::MPMCQueue<void*> queue_compress(QUEUE_SIZE);
 folly::MPMCQueue<void*> queue_reorder(QUEUE_SIZE);
-
-int enqueue_from_ringbuffer(folly::MPMCQueue<void*>& queue, ringbuffer_t* buf,
-                            int num) {
-  int i;
-
-  // Try to enqueue up to num elements
-  for (i = 0; i < num && !ringbuffer_isEmpty(buf); i++) {
-
-    // Peek at the ringbuffer tail and attempt to enqueue
-    void* tmp = ringbuffer_peek(buf);
-    assert(tmp != NULL);
-
-    // If write succeeds, remove from ringbuffer
-    if (queue.write(tmp)) {
-      ringbuffer_remove(buf);
-    }
-    // Otherwise, keep in ring buffer and break
-    else {
-      break;
-    }
-  }
-
-  return i;
-}
-
-int dequeue_to_ringbuffer(folly::MPMCQueue<void*>& queue, ringbuffer_t* buf,
-                          int num) {
-  int i;
-
-  for (i = 0; i < num && !ringbuffer_isFull(buf); i++) {
-    void* tmp;
-
-    if (queue.read(tmp)) {
-      if (tmp == nullptr) {
-        // Enqueue two more sentinel nullptrs so we shutdown in O(log C) instead
-        // of O(C)
-        // TODO: check return values?
-        queue.write(nullptr);
-        queue.write(nullptr);
-        return -1;
-      }
-      assert(tmp != NULL);
-      int r = ringbuffer_insert(buf, tmp);
-      assert(r >= 0);
-    } else {
-      break;
-    }
-  }
-
-  return i;
-}
-
-void terminate_queue(folly::MPMCQueue<void*>& queue) {
-  // TODO: do we want to drain the queue as well?
-  // TODO: verify there's room to write the null pointer
-  // TODO: check return value
-  queue.write(nullptr);
-}
 
 // The configuration block defined in main
 extern config_t* conf;
